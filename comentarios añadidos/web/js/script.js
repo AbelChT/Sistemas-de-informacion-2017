98ae427d3c035c9116230091436e95df63a8365d
Abel <abelcht@outlook.es>
2017-11-24 01:09:08 +0100

    comentarios

---
+++ comentarios añadidos/web/js/script.js
@@ -0,0 +1,21 @@
+$(".single-item").slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000
+});
+
+$(function () {
+    $('a[href="#search"]').on('click', function(event) {
+        event.preventDefault();
+        $('#search').addClass('open');
+        $('#search > form > input[type="search"]').focus();
+    });
+
+    $('#search, #search button.close').on('click keyup', function(event) {
+        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
+            $(this).removeClass('open');
+        }
+    });
+});
